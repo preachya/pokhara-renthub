419ad10863a7797d2d70e8cca6c0e67c90eb7e10
preachya <preachyamulmi@gmail.com>
2025-02-22 19:33:18 +0545

    initial of js

---
+++ script.js
@@ -0,0 +1,8 @@
+// Mobile Navigation
+const hamburger = document.querySelector('.hamburger');
+const navLinks = document.querySelector('.nav-links');
+
+hamburger.addEventListener('click', () => {
+    navLinks.classList.toggle('active');
+});
+
